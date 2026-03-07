4aa47b5f3919c26e7c7c37ab9675592b96f4ba1f
NISCHAY KUMAR <NischayTech@gmail.com>
2026-03-07 11:22:42 +0530

    more on camera

--- zelda-attempt/ENTITIES/player/camera_controller.gd
+++ zelda-attempt/ENTITIES/player/camera_controller.gd
@@ -1,11 +1,14 @@
 extends Node3D
 
+@export var min_limit_x: float
+@export var max_limit_x: float
+@export var mouse_acceleration := 0.005
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		rotate_from_vector(event.relative * 0.0067)
+		rotate_from_vector(event.relative * mouse_acceleration)
 		
 func rotate_from_vector(v: Vector2):
 	if v.length() == 0: return
 	rotation.y -= v.x
 	rotation.x -= v.y * 0.1
-	rotation.x = clamp(rotation.x, -0.4, 0.3)
+	rotation.x = clamp(rotation.x, min_limit_x, max_limit_x)
