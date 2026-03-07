bfa8725a7e02e2a1ef214770a207e14bd371364c
NISCHAY KUMAR <NischayTech@gmail.com>
2026-03-07 17:19:16 +0530

    hi godette

--- zelda-attempt/ENTITIES/player/player.gd
+++ zelda-attempt/ENTITIES/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-#jump
+# jump settings
 @export var jump_height : float = 2.25
 @export var jump_time_to_peak : float = 0.4
 @export var jump_time_to_descent : float = 0.3
@@ -10,10 +10,12 @@ extends CharacterBody3D
 @onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
 @onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
-@export var base_speed := 4.0
-@export var run_speed := 2005.0
+# movement speeds
+@export var base_speed := 6.0
+@export var run_speed := 25.0
 @onready var camera_3d: Node3D = $"CAMERA CONTROLLER"
 
+# movement direction input
 var movement_input := Vector2.ZERO
 
 
@@ -23,14 +25,19 @@ func jump_logic(delta) -> void:
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = -jump_velocity
 
+	# apply gravity
 	var gravity = jump_gravity if velocity.y > 0 else fall_gravity
 	velocity.y -= gravity * delta
 	
 
 
 func _physics_process(delta: float) -> void:
-	movement_input = Input.get_vector("left","right","forward","backward").rotated(-camera_3d.global_rotation.y)
-	velocity = Vector3(movement_input.x,0,movement_input.y) * base_speed
+	# read input relative to camera
+	# old move code
+	# movement_input = Input.get_vector("left","right","forward","backward").rotated(-camera_3d.global_rotation.y)
+	
+	#velocity = Vector3(movement_input.x,0,movement_input.y) * base_speed
+	
 	move_logic(delta)
 	jump_logic(delta)
 	move_and_slide()
@@ -41,26 +48,34 @@ func _physics_process(delta: float) -> void:
 
 func move_logic(delta) -> void:
 	movement_input = Input.get_vector("left","right","forward","backward").rotated(-camera_3d.global_rotation.y)
+	
+	# convert horizontal velocity to 2d
 	var vel_2d = Vector2(velocity.x, velocity.z)
-	print("move_logic running")
+	#print("move_logic running")
 	
 
-
 	var is_running: bool = Input.is_action_pressed("run")
-	print(is_running)
+	#print(is_running)
 	
 
 	if movement_input != Vector2.ZERO:
+		# choose speed depending on run key
 		var speed = run_speed if is_running else base_speed
-		print(speed)
+		#print(speed)
+
+		# run anim
+		$godetteSkin/AnimationPlayer.current_animation = 'Running_B'
 		vel_2d += movement_input * speed * delta
 		vel_2d = vel_2d.limit_length(speed)
+
 		velocity.x = vel_2d.x
 		velocity.z = vel_2d.y
-	
+		var target_angle = movement_input.angle()
+		$godetteSkin.rotation.y = target_angle
 	else:
+		# slow down when no input
 		vel_2d = vel_2d.move_toward(Vector2.ZERO, base_speed * 4 * delta)
-
-	velocity.x = vel_2d.x
-	velocity.z = vel_2d.y
-	
+		velocity.x = vel_2d.x
+		velocity.z = vel_2d.y
+		# walk anim
+		$godetteSkin/AnimationPlayer.current_animation = 'Idle'
