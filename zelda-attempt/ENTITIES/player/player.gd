90a7d3ffb4a1c9463429c3df0cf3647998b87849
NISCHAY KUMAR <NischayTech@gmail.com>
2026-03-07 16:18:25 +0530

    move work

--- zelda-attempt/ENTITIES/player/player.gd
+++ zelda-attempt/ENTITIES/player/player.gd
@@ -6,19 +6,24 @@ extends CharacterBody3D
 @export var jump_time_to_peak : float = 0.4
 @export var jump_time_to_descent : float = 0.3
 
-@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak ) * -1.0
-@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)   )
-@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent )   )
+@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
 @export var base_speed := 4.0
+@export var run_speed := 2005.0
 @onready var camera_3d: Node3D = $"CAMERA CONTROLLER"
 
 var movement_input := Vector2.ZERO
 
+
+
 func jump_logic(delta) -> void:
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = -jump_velocity
-	var gravity = jump_gravity if  velocity .y > 0 else fall_gravity
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = -jump_velocity
+
+	var gravity = jump_gravity if velocity.y > 0 else fall_gravity
 	velocity.y -= gravity * delta
 	
 
@@ -26,8 +31,36 @@ func jump_logic(delta) -> void:
 func _physics_process(delta: float) -> void:
 	movement_input = Input.get_vector("left","right","forward","backward").rotated(-camera_3d.global_rotation.y)
 	velocity = Vector3(movement_input.x,0,movement_input.y) * base_speed
-	move_and_slide()
+	move_logic(delta)
 	jump_logic(delta)
+	move_and_slide()
+
+	
+
+	
+
+func move_logic(delta) -> void:
+	movement_input = Input.get_vector("left","right","forward","backward").rotated(-camera_3d.global_rotation.y)
+	var vel_2d = Vector2(velocity.x, velocity.z)
+	print("move_logic running")
+	
 
 
+	var is_running: bool = Input.is_action_pressed("run")
+	print(is_running)
+	
+
+	if movement_input != Vector2.ZERO:
+		var speed = run_speed if is_running else base_speed
+		print(speed)
+		vel_2d += movement_input * speed * delta
+		vel_2d = vel_2d.limit_length(speed)
+		velocity.x = vel_2d.x
+		velocity.z = vel_2d.y
+	
+	else:
+		vel_2d = vel_2d.move_toward(Vector2.ZERO, base_speed * 4 * delta)
+
+	velocity.x = vel_2d.x
+	velocity.z = vel_2d.y
 	
