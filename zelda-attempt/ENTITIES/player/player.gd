52ad93b098d5e625af7371a513bff78782af0e44
NISCHAY KUMAR <NischayTech@gmail.com>
2026-03-07 11:48:14 +0530

    gave the bean some glasses

--- zelda-attempt/ENTITIES/player/player.gd
+++ zelda-attempt/ENTITIES/player/player.gd
@@ -1,13 +1,27 @@
 extends CharacterBody3D
+
+
+#jump
+@export var jump_height : float = 2.25
+@export var jump_time_to_peak : float = 0.4
+@export var jump_time_to_descent : float = 0.3
+
+@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak ) * -1.0
+@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)   )
+@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent )   )
+
 @export var base_speed := 4.0
-
-
 @onready var camera_3d: Node3D = $"CAMERA CONTROLLER"
 
 var movement_input := Vector2.ZERO
 func _physics_process(_delta: float) -> void:
 	movement_input = Input.get_vector("left","right","forward","backward").rotated(-camera_3d.global_rotation.y)
 	velocity = Vector3(movement_input.x,0,movement_input.y) * base_speed
+	
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = 4
+		
+	
 	move_and_slide()
 	
 	
