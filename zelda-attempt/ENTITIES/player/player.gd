fc68ea15933159791283c11ae0ad35fe31408283
NISCHAY KUMAR <NischayTech@gmail.com>
2026-03-07 12:22:15 +0530

    fine tuning jump

--- zelda-attempt/ENTITIES/player/player.gd
+++ zelda-attempt/ENTITIES/player/player.gd
@@ -14,14 +14,20 @@ extends CharacterBody3D
 @onready var camera_3d: Node3D = $"CAMERA CONTROLLER"
 
 var movement_input := Vector2.ZERO
-func _physics_process(_delta: float) -> void:
+
+func jump_logic(delta) -> void:
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_velocity
+	var gravity = jump_gravity if  velocity .y > 0 else fall_gravity
+	velocity.y -= gravity * delta
+	
+
+
+func _physics_process(delta: float) -> void:
 	movement_input = Input.get_vector("left","right","forward","backward").rotated(-camera_3d.global_rotation.y)
 	velocity = Vector3(movement_input.x,0,movement_input.y) * base_speed
-	
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = 4
-		
-	
 	move_and_slide()
-	
+	jump_logic(delta)
+
+
 	
