e191459905abcee30496866e960e644a12e530f5
NISCHAY KUMAR <NischayTech@gmail.com>
2026-03-07 10:06:26 +0530

    getting started for the day

--- zelda-attempt/ENTITIES/camera_controller.gd
+++ zelda-attempt/ENTITIES/camera_controller.gd
@@ -2,9 +2,9 @@ extends Node3D
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		rotation.y += event.relative.x * 0.0067
+		rotate_from_vector(event.relative * 0.0067)
 		
 func rotate_from_vector(v: Vector2):
 	if v.length() == 0: return
-	rotation.y += v.x
+	rotation.y -= v.x
 	
--- zelda-attempt/ENTITIES/player/player.gd
+++ zelda-attempt/ENTITIES/player/player.gd
@@ -6,5 +6,4 @@ var movement_input := Vector2.ZERO
 func _physics_process(delta: float) -> void:
 	movement_input = Input.get_vector("left","right","forward","backward")
 	velocity = Vector3(movement_input.x,0,movement_input.y) * base_speed
-	print(movement_input)
 	move_and_slide()
