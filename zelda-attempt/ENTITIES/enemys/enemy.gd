e85cb39a289e240454815ddd591a62b6be384aed
NISCHAY KUMAR <NischayTech@gmail.com>
2026-03-15 00:08:54 +0530

    work on making boss move with anim

--- zelda-attempt/ENTITIES/enemys/enemy.gd
+++ zelda-attempt/ENTITIES/enemys/enemy.gd
@@ -1,6 +1,6 @@
 class_name Enemy
 extends CharacterBody3D
-
+@onready var move_state_machine = $AnimationTree.get("parameters/MoveStateMachine/playback")
 @onready var player = get_tree().get_first_node_in_group('Player')
 @onready var skin = get_node('skin')
 @export var notice_radius := 30.0
@@ -12,6 +12,11 @@ func move_to_player(delta):
 		var target_vec2 = Vector2(target_dir.x, target_dir.z   )
 		var target_angle = -target_vec2.angle() + PI/2
 		rotation.y = rotate_toward(rotation.y, target_angle, delta * 6.7)
-		velocity = Vector3(target_vec2.x, 0 , target_vec2.y) * walk_speed
+		if position.distance_to(player.position) > attack_radius:
+			velocity = Vector3(target_vec2.x, 0 , target_vec2.y) * walk_speed
+			move_state_machine.travel('walk')
+		else:
+			velocity = Vector3.ZERO
+			move_state_machine.travel('idle')
 		move_and_slide()
 	
