0d1a4031d4b27b561ea068bda64efec91202d7f0
NISCHAY KUMAR <NischayTech@gmail.com>
2026-03-24 18:52:30 +0530

    getting started

--- zelda-attempt/ENTITIES/enemys/boss.gd
+++ zelda-attempt/ENTITIES/enemys/boss.gd
@@ -1,21 +1,22 @@
 extends Enemy
-
 const simple_attacks = {
 	'slice' : "2H_Melee_Attack_Slice",
 	'spin' : "2H_Melee_Attack_Spin" ,
 	'range' : "1H_Melee_Attack_Stab" , 
 }
-
 @export var spin_speed = 6
 var spinning := false
 func  _physics_process(delta: float) -> void:
 	move_to_player(delta)
 func _on_attack_timer_timeout() -> void:
+	$Timers/AttackTimer.wait_time - rng.randf_range(1.0,5.6)
 	if position.distance_to(player.position) < 5.0:	
 		melee_attack_animation()
 	else:
-		#range_attack_animation()
-		spin_attack_animation()
+		if rng.randi() % 2:
+			range_attack_animation()
+		else:
+			spin_attack_animation()
 		# 4 anim
 		# 2 melee attacks
 		# 2 range attacks
@@ -25,8 +26,6 @@ func spin_attack_animation():
 	tween.tween_method(_spin_transition, 0.0, 1.0, 0.3 )
 	$Timers/AttackTimer.stop()
 	spinning = true
-	
-	
 func _spin_transition(value: float) -> void:
 	$AnimationTree.set("parameters/SpinBlend/blend_amount", value)
 func range_attack_animation():
@@ -36,7 +35,11 @@ func range_attack_animation():
 func melee_attack_animation():
 	attack_animation.animation = simple_attacks['slice' if rng.randi() % 2 else 'spin']
 	$AnimationTree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
-
-
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	print(body)
+	if spinning:
+		await get_tree().create_timer(rng.randf_range(1.0,2.3)).timeout
+		var tween = create_tween()
+		tween.tween_property(self,"speed",walk_speed,0.5 )
+		tween.tween_method(_spin_transition, 1.0, 0.0, 0.3 )
+		spinning = false
+		$Timers/AttackTimer.start()
