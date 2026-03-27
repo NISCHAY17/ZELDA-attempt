d8cb0f0ee3d1277902dfd940beb78a57b2e0a87a
NISCHAY KUMAR <NischayTech@gmail.com>
2026-03-27 20:09:08 +0530

    MORE WORK

--- zelda-attempt/ENTITIES/enemys/skeleton_mage.gd
+++ zelda-attempt/ENTITIES/enemys/skeleton_mage.gd
@@ -1,4 +1,14 @@
 extends Enemy
 
+func _ready() -> void:
+	attack_radius = 10.0
+
+
 func _physics_process(delta: float) -> void:
 	move_to_player(delta)
+
+
+func _on_attack_timer_timeout() -> void:
+	$Timers/AttackTimer.wait_time  = rng.randf_range(1.5,3.5)
+	if position.distance_to(player.position) < attack_radius:
+		$AnimationTree.set("parameters/OneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE )  
