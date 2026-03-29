32b72ffe06dd424abbbf175bf38f5baa805fbc0b
NISCHAY KUMAR <NischayTech@gmail.com>
2026-03-29 19:53:56 +0530

    made the apell cast shoot work

--- zelda-attempt/ENTITIES/enemys/skeleton_mage.gd
+++ zelda-attempt/ENTITIES/enemys/skeleton_mage.gd
@@ -9,10 +9,15 @@ func _physics_process(delta: float) -> void:
 	move_to_player(delta)
 
 func _on_attack_timer_timeout() -> void:
-	$Timers/AttackTimer.wait_time  = rng.randf_range(2.5,3.5)
+	$Timers/AttackTimer.wait_time  = rng.randf_range(2.5,4.67)
+	#print($Timers/AttackTimer.wait_time )
 	if position.distance_to(player.position) < attack_radius:
-		$AnimationTree.set("parameters/OneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE   )
+		#print('attack time')
+		$AnimationTree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE   )
 
 func shoot_fireball() -> void:
-	cast_spell.emit('fireball', $skin/Rig/Skeleton3D/BoneAttachment3D/wand2/Marker3D, global_position, Vector2.RIGHT, 1.0)
+	var direction = (player.position - position).normalized()
+	var dir_2d = Vector2(direction.x, direction.z)
+	var pos = $skin/Rig/Skeleton3D/BoneAttachment3D/wand2/Marker3D.global_position
+	cast_spell.emit('fireball', pos, dir_2d, 1.0)
  
