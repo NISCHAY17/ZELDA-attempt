f8f2319ebba8c4cde8ac869bc88033905027e064
NISCHAY KUMAR <NischayTech@gmail.com>
2026-04-12 19:25:13 +0530

    added killzone

--- zelda-attempt/scenes/dungeon.gd
+++ zelda-attempt/scenes/dungeon.gd
@@ -3,3 +3,11 @@ extends Level
 
 func _on_doorarea_body_entered(body: Node3D) -> void:
 	switch_level('overworld')
+
+
+
+
+
+func _on_area_3d_2_body_entered(body: Node3D) -> void:
+	print("killzoneenter")
+	get_tree().reload_current_scene()
